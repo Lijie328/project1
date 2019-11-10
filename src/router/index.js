49633cc8736c2765ddca214aa1ac4398afb4fbbc
Lijie328 <1776645662@qq.com>
2019-11-10 20:48:15 +0800

    home完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,24 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/login/index.vue'
 import Home from '../views/home/index.vue'
+import Welcom from '../views/home/welcome/index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   // { path: '/login', name: 'login', component: () => import('@/views/login/index.vue') }
   { path: '/login', name: 'login', component: Login },
-  { path: '/home', name: 'home', component: Home }
+  {
+    path: '/home',
+    name: 'home',
+    component: Home,
+    redirect: '/welcome', // 路由重定向
+    children: [
+      // 欢迎页面子路由配置
+      { path: '/welcome', name: 'welcome', component: Welcom }
+    ]
+  }
+
 ]
 
 const router = new VueRouter({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,145 @@
 <template>
-  <div>
-      <h3>后台首页</h3>
-  </div>
+  <el-container>
+    <el-aside :width="isCollapse?'65px':'200px'">
+      <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu-item
+          index="1"
+          style="width:200px"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
+          <i class="el-icon-location"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-submenu index="2" :style=" {width :isCollapse?'65px':'200px' }">
+          <template slot="title">
+            <i class="el-icon-menu"></i>
+            <span slot="title">内容管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="2-1">发布文章</el-menu-item>
+            <el-menu-item index="2-2">内容列表</el-menu-item>
+            <el-menu-item index="2-3">评论列表</el-menu-item>
+            <el-menu-item index="2-4">素材管理</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-menu-item index="3" :style=" {width :isCollapse?'65px':'200px' }">
+          <i class="el-icon-location"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+
+        <el-menu-item index="4" :style=" {width :isCollapse?'65px':'200px' }">
+          <i class="el-icon-location"></i>
+          <span slot="title">账户信息</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <div id="lt">
+          <i
+            class="el-icon-s-fold"
+            slot="prefix"
+            :class="isCollapse?'el-icon-s-unfold':'el-icon-s-fold'"
+            style="cursor:pointer;"
+            @click="isCollapse=!isCollapse"
+          ></i>
+          <span>江苏传智播客教育科技股份有限公司</span>
+        </div>
+        <div id="rt">
+          <el-input type="text" placeholder="请输入搜索的文章内容">
+            <i slot="prefix" class="el-input__icon el-icon-search"></i>
+          </el-input>
+          <span style="margin:0 10px;">消息</span>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              <img :src="photo" alt width="40" height="40" />
+              {{name}}
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人信息</el-dropdown-item>
+              <el-dropdown-item>git地址</el-dropdown-item>
+              <el-dropdown-item @click.native="loginout()">退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      isCollapse: false
+    }
+  },
+  computed: {
+    name () {
+      return JSON.parse(window.sessionStorage.getItem('userinfo')).name
+    },
+    photo () {
+      return JSON.parse(window.sessionStorage.getItem('userinfo')).photo
+    }
+  },
+  methods: {
+    loginout () {
+      this.$confirm('确定要提出系统?', '退出', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        window.sessionStorage.clear()
+        this.$router.push('/login')
+      }).catch(() => { })
+    }
+  }
 }
+
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+  .el-aside {
+    height: 100%;
+    background-color: #456adf;
+  }
+  .el-header {
+    padding: 0 10px 0 9px;
+    min-width: 950px;
+    background-color: #0f0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    #lt {
+      height: 100%;
+      width: 40%;
+      background-color: white;
+      font-size: 20px;
+      display: flex;
+      align-items: center;
+    }
+    #rt {
+      height: 100%;
+      width: 50%;
+      background-color: white;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      .el-dropdown-link {
+        display: flex;
+        align-items: center;
+      }
+    }
+  }
+  .el-main {
+    background-color: #515153;
+  }
+}
 </style>
